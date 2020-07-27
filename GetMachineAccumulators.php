116d4b087c06c89337edca75622f51c7848da0f8
Herman Ras <hermanr@PetraGroup.local>
2020-07-27 11:28:20 +0200

    working Accumulators API

--- GetMachineAccumulators.php
+++ GetMachineAccumulators.php
@@ -11,21 +11,6 @@ $machines = array(
     '8999208300'
 );
 
-// //setup Start Parameters
-// $StartDate = new DateTime();
-// $StartDate = $StartDate->format('Y-m-1');
-// if(isset($_GET['StartDate'])){
-//     $StartDate = $_GET['StartDate'];   
-// };
-
-
-// //setup Stop Parameters
-// $StopDate = new DateTime();
-// $StopDate = $StopDate->format('Y-m-t');
-// if(isset($_GET['StartDate'])){
-//   $StartDate = $_GET['StartDate'];
-// };
-
 foreach ($machines as $machinesId) {
     $url = $BaseURL.$machinesId."/machineAccumulators";
     $HEAD_Data = array(
@@ -35,65 +20,61 @@ foreach ($machines as $machinesId) {
     $POST_Data = array();
     $data = curl($url,$HEAD_Data,$POST_Data);    
     $data = json_decode($data,true);
-    
-    echo json_encode($data);
-    die;
-    
-    foreach ($data as $rec) {
-        $sql = "INSERT INTO [dbo].[KPIs]
-                    (ItemNumber,
-                    date,
-                    dailyDrillHours,
-                    dailyDrillMeters,
-                    dailyDrillMetersPerEngineHour,
-                    dailyDrillMetersPerDrillHour,
-                    dailyDrillHoursPerEngineHour,
-                    dailyDrillHoles,
-                    dailyFuelLiters,
-                    dailyFuelLitersPerHour,
-                    dailyFuelCO2Emission,
-                    dailyFuelLitersPerTonnes,
-                    dailyFuelLitersPerMeter,
-                    dailyLoadingTonnes,
-                    dailyLoadingTonnesPerHour,
-                    dailyLoadingNumberOfBuckets,
-                    dailyLoadingTonnesPerBucket,
-                    dailyLoadingNumberOfBoxes,
-                    dailyLoadingTonnesPerBox,
-                    dailyUtilizationAvailableHours,
-                    dailyUtilizationWorkedHours,
-                    dailyUtilizationDrillHours,
-                    dailyUtilizationHydraulicPumpHours,
-                    dailyUtilizationTrammingHours,
-                    dailyUtilizationIdleHours,
-                    dailyUtilizationEngineHours) 
+
+    foreach ($data as  $key => $rec) {
+        // if Rec has Node
+        if (count($rec['nodes'])!== 0){
+            // Select Top 1000
+            //   nodes.ID,
+            //   nodes.nodeIndex,
+            //   nodes.value,
+            //   nodes.timeStamp
+            // From
+            //   nodes
+            $sql = "INSERT INTO [dbo].[nodes]
+                     (nodeIndex,
+                      value,
+                      timeStamp) 
+                    VALUES
+                     ('".$rec['nodes'][0]["nodeIndex"]."',
+                     '".$rec['nodes'][0]["value"]."',
+                     '".$rec['nodes'][0]["timeStamp"]."');";
+
+            $sqlargs = array();
+            require_once 'config/db_query.php'; 
+            sqlQuery($sql,$sqlargs);
+
+            // Get last Node ID for KPI
+            $sql = "SELECT TOP 1 [MRPSolutionDB].[dbo].[nodes].ID 
+                    FROM [MRPSolutionDB].[dbo].[nodes]
+                    ORDER BY ID DESC";
+            require_once 'config/db_query.php'; 
+            $nodeID = sqlQuery($sql,$sqlargs);
+            $nodeID = $nodeID[0][0]["ID"];
+        }else{
+            $nodeID = '';
+        }
+        
+        //   CumulativeKPIs.Nodes,
+        //   CumulativeKPIs.Description,
+        //   CumulativeKPIs.value,
+        //   CumulativeKPIs.MachineID,
+        //   CumulativeKPIs.timeStamp
+        // From
+        //   CumulativeKPIs
+
+        $sql = "INSERT INTO [dbo].[CumulativeKPIs]
+                    (Nodes,
+                    Description,
+                    value,
+                    MachineID,
+                    timeStamp) 
                 VALUES
-                    ('".$machinesId."',
-                    '".$rec["date"]."',
-                    '".$rec["dailyDrillHours"]."',
-                    '".$rec["dailyDrillMeters"]."',
-                    '".$rec["dailyDrillMetersPerEngineHour"]."',
-                    '".$rec["dailyDrillMetersPerDrillHour"]."',
-                    '".$rec["dailyDrillHoursPerEngineHour"]."',
-                    '".$rec["dailyDrillHoles"]."',
-                    '".$rec["dailyFuelLiters"]."',
-                    '".$rec["dailyFuelLitersPerHour"]."',
-                    '".$rec["dailyFuelCO2Emission"]."',
-                    '".$rec["dailyFuelLitersPerTonnes"]."',
-                    '".$rec["dailyFuelLitersPerMeter"]."',
-                    '".$rec["dailyLoadingTonnes"]."',
-                    '".$rec["dailyLoadingTonnesPerHour"]."',
-                    '".$rec["dailyLoadingNumberOfBuckets"]."',
-                    '".$rec["dailyLoadingTonnesPerBucket"]."',
-                    '".$rec["dailyLoadingNumberOfBoxes"]."',
-                    '".$rec["dailyLoadingTonnesPerBox"]."',
-                    '".$rec["dailyUtilizationAvailableHours"]."',
-                    '".$rec["dailyUtilizationWorkedHours"]."',
-                    '".$rec["dailyUtilizationDrillHours"]."',
-                    '".$rec["dailyUtilizationHydraulicPumpHours"]."',
-                    '".$rec["dailyUtilizationTrammingHours"]."',
-                    '".$rec["dailyUtilizationIdleHours"]."',
-                    '".$rec["dailyUtilizationEngineHours"]."');";
+                    ('". $nodeID ."',
+                    '". $key ."',
+                    '".$rec["totalValue"]."',
+                    '".$machinesId."',
+                    '".$rec ["timeStamp"]."');";
 
             $sqlargs = array();
             require_once 'config/db_query.php'; 
